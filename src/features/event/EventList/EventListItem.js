1c00e3188febf666c19f095fafb40fc84f145fe2
John Nam <john@Johnui-MacBook-Pro.local>
2019-03-07 15:16:50 -0500

    CRUD

--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -4,7 +4,8 @@ import EventListAttendee from './EventListAttendee'
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    
+    const {event, onEventOpen, deleteEvent} = this.props;
 
     return (
            <Segment.Group>
@@ -15,7 +16,7 @@ class EventListItem extends Component {
                     <Item.Content>
                       <Item.Header as="a">{event.title}</Item.Header>
                       <Item.Description>
-                        Hosted by <a>{event.hostedBy}</a>
+                        Hosted by <p>{event.hostedBy}</p>
                       </Item.Description>
                     </Item.Content>
                   </Item>
@@ -29,14 +30,15 @@ class EventListItem extends Component {
               </Segment>
               <Segment secondary>
                 <List horizontal>
-                  {event.attendees.map((attendee) => {
+                  {event.attendees && event.attendees.map((attendee) => {
                     return <EventListAttendee key={attendee.id} attendee={attendee}/>
                   })}
                 </List>
               </Segment>
               <Segment clearing>
                 <span>{event.description}</span>
-                <Button as="a" color="teal" floated="right" content="View" />
+                <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
+                <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
               </Segment>
             </Segment.Group>
     )
